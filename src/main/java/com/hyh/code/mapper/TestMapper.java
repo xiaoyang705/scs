ae5303deb0f0327a58739c13a07b1a2333868462
xiaoyang705 <huyuehuang0322@126.com>
2021-03-14 15:33:39 +0800

    20210313晚上

--- src/main/java/com/hyh/code/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hyh.code.mapper;
-
-import com.hyh.code.base.IBaseMapper;
-import com.hyh.code.pojo.Test;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper extends IBaseMapper<Test> {
-}
--- src/main/java/com/hyh/code/service/impl/TestService.java
+++ src/main/java/com/hyh/code/service/impl/TestService.java
@@ -1,8 +1,6 @@
 package com.hyh.code.service.impl;
 
-import com.hyh.code.mapper.TestMapper;
 import com.hyh.code.pojo.Test;
-import com.hyh.code.service.ITestService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
